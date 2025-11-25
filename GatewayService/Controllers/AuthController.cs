d10bd74fd1522f5ec7f2accb445213b1ec7f2cc3
Макс Михайлов <fsb-kruto@6otdel-voobshe-mechta.ru>
2025-11-25 15:00:51 +0300

    Add AuthController plug in GatewayService

---
+++ GatewayService/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayService.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        try
+        {
+            return Ok(new
+            {
+
+            });
+        }
+        catch (RpcException ex) when (ex.StatusCode == Grpc.Core.StatusCode.Unauthenticated)
+        {
+            return Unauthorized(new
+            {
+                message = "Invalid credentials"
+            });
+        }
+    }
+}
--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSwaggerGen()
+builder.Services.AddControllers();
+//builder.Services.AddHttpClient(); AddSwagger
+
+//builder.Services.AddGrpcClient<AuthServices.AuthServiceClient>(options =>)
+//builder.Services.AddGrpcClient<AuthServices.UserServiceClient>(options =>)
+//builder.Services.AddGrpcClient<AuthServices.GradeServiceClient>(options =>)
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+//app.UseSwagger();
+//app.UseSwaggerUI();
 
 app.Run();
